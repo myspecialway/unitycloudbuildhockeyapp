7f3168280c72d6d8d44dd1c8779e50b4f2c98e90
Buslik, Raz (rb580s) <rb580s@intl.att.com>
2018-09-30 08:52:16 +0300

    Update code:
    * Use Express' JSON body parser.
    * Add security validation of build requests.
    * Updated dependencies.

--- index.js
+++ index.js
@@ -2,54 +2,40 @@
 require('dotenv').config();
 
 // Options
-var options = {
-  port: process.env.PORT || 80, // Heroku port or 80.
-  unityAPIBase: "https://build-api.cloud.unity3d.com/", // URI (e.g. href) recieved in web hook payload.
-  unityCloudAPIKey: process.env.UNITYCLOUD_KEY,
-  hockeyappAPIUpload: "https://rink.hockeyapp.net/api/2/apps/upload",
-  hockeyappAPIKey: process.env.HOCKEYAPP_KEY
+const options = {
+    port: process.env.PORT || 3000,
+    unityAPIBase: "https://build-api.cloud.unity3d.com", // URI (e.g. href) recieved in web hook payload.
+    unityCloudAPIKey: process.env.UNITYCLOUD_KEY,
+    hockeyappId: process.env.HOCKEYAPPID,
+    hockeyappAPIKey: process.env.HOCKEYAPP_KEY,
+    authorizationKey: process.env.AUTH_KEY,
+    projectGuid: process.env.PROJECT_GUID
 };
 
 // Imports
-var path = require('path'),
+const path = require('path'),
     fs = require('fs'),
+    https = require('https'),
     express = require('express'),
     app = express(),
-    http = require('http'),
-    https = require('https'),
-    server = http.Server(app),
-    bodyParser = require('body-parser'),
-    najax = require('najax'),
+    request = require('request'),
     FormData = require('form-data'),
-    _ = require('lodash'),
-    url = require("url"),
-    path = require("path");
+    url = require("url");
+
+app.use(express.json());
 
 // Run Server
-var server = server.listen( options.port, function(){
+app.listen( options.port, function(){
   console.log('listening on *:' + options.port );
 });
 
-// Configure Express
-app.use('/public', express.static('public'));
-
-// parse application/json
-// app.use(bodyParser.json()); // Parse all
-var jsonParser = bodyParser.json();
-
-app.get('/', function(req, res){
-  res.sendFile( __dirname + '/index.html' );
-});
-
-// POST /api/users gets JSON bodies 
-var mainRes;
-app.post('/build', jsonParser, function (req, res) {
+app.post('/build', function (req, res) {
     if (!req.body) return res.sendStatus(400);
 
-    mainRes = res;
+    if(!isValidRequest(req)) return res.sendStatus(401);
 
     // 1. Get Build API URL
-    var buildAPIURL = req.body.links.api_self.href;
+    const buildAPIURL = req.body.links.api_self.href;
     if( !buildAPIURL ) {
         // URL not available.
         res.setHeader('Content-Type', 'application/json');
@@ -58,52 +44,70 @@ app.post('/build', jsonParser, function (req, res) {
             message: "No build link from Unity Cloud Build webhook"
         });
     } else {
-        // URL not available.
+        // URL available.
         res.setHeader('Content-Type', 'application/json');
         res.send({
             error: false,
             message: "Process begun for project '" + req.body.projectName + "' platform '" + req.body.buildTargetName + "'."
         });
+
+        // 2. Grab binary URL from Unity Cloud API
+        getBuildDetails( buildAPIURL );
+    }
+});
+
+function isValidRequest(req){
+
+    let authHeader = req.header("Authorization");
+    if(authHeader !== options.authorizationKey) {
+        console.log(`Invalid header - ${authHeader}`);
+        return false;
     }
 
-    // 2. Grab binary URL from Unity Cloud API
-    getBuildDetails( buildAPIURL );
-});
+    if(req.body.projectGuid !== options.projectGuid){
+        console.log(`Invalid project guid - ${req.body.projectGuid}`);
+        return false;
+    }
+
+    if(req.body.buildStatus !== 'success'){
+        console.log(`Invalid buildStatus - ${req.body.buildStatus}`);
+        return false;
+    }
+
+    return true;
+}
 
 function getBuildDetails( buildAPIURL ){
     console.log("1. getBuildDetails: start");
 
-    najax({ 
-        url: options.unityAPIBase + buildAPIURL,
-        type: 'GET', 
+    const requestOptions = {
         headers: {
-            'Authorization': 'Basic ' + options.unityCloudAPIKey
-        },
-        success: function(data){
-
-            var data = JSON.parse(data);
-            // console.log( data.links.download_primary.href );
-
-            var parsed = url.parse( data.links.download_primary.href );
-            var filename = path.basename( parsed.pathname );
-
-            console.log("1. getBuildDetails: finished");
-
-            // 3. Download binary.
-            downloadBinary( data.links.download_primary.href, filename );
-
-        },
-        error: function(error){
-            console.log(error);
-
-            mainRes.setHeader('Content-Type', 'application/json');
-            mainRes.send({
-                error: true,
-                message: "Problem getting build details from Unity Cloud Build.",
-                errorDump: error
-            });
+            'Authorization': `Basic ${options.unityCloudAPIKey}`,
+            'Content-Type': 'application/json'
         }
-    });
+    };
+
+    request.get(options.unityAPIBase + buildAPIURL, requestOptions, function(error, response, body){
+
+            if(error) {
+                console.log(error);
+            }
+            else if(response.statusCode !== 200) {
+                console.log(`Failed getting details with status - ${response.statusCode}`);
+            }
+            else {
+                const data = JSON.parse(body);
+
+                const parsed = url.parse(data.links.download_primary.href);
+                const filename = path.basename(parsed.pathname);
+
+                console.log("1. getBuildDetails: finished");
+
+                // 3. Download binary.
+                downloadBinary(data.links.download_primary.href, filename);
+            }
+        }
+    );
 }
 
 function downloadBinary( binaryURL, filename ){
@@ -113,19 +117,21 @@ function downloadBinary( binaryURL, filename ){
     console.log("   " + filename);
 
     deleteFile( filename );
-    
+
     https.get( binaryURL, (res) => {
-        // console.log('statusCode: ', res.statusCode);
-        // console.log('headers: ', res.headers);
 
-        var writeStream = fs.createWriteStream(filename, {'flags': 'a'});
+        if(res.statusCode !== 200){
+            return console.log(`Failed to download with status - ${res.statusCode}`);
+        }
 
-        var len = parseInt(res.headers['content-length'], 10);
-        var cur = 0;
-        var total = len / 1048576; //1048576 - bytes in  1Megabyte
+        const writeStream = fs.createWriteStream(filename, {'flags': 'a'});
+
+        const len = parseInt(res.headers["content-length"], 10);
+        let cur = 0;
+        const total = len / 1048576; //1048576 - bytes in  1Megabyte
 
         res.on('data', (chunk) => {
-        
+
             cur += chunk.length;
             writeStream.write(chunk, 'binary');
 
@@ -141,7 +147,6 @@ function downloadBinary( binaryURL, filename ){
 
         writeStream.on('finish', () => {
 
-            // console.log("2. downloadBinary: file finished");          
             uploadToHockeyApp( filename );
         });
 
@@ -152,55 +157,49 @@ function downloadBinary( binaryURL, filename ){
 
 function uploadToHockeyApp( filename ){
     console.log("3. uploadToHockeyApp: start");
-    // console.log("readfile: " + filename);
 
-    var readable = fs.createReadStream( filename );
+    const readable = fs.createReadStream(filename);
     readable.on('error', () => {
-      console.log('Error reading binary file for upload to HockeyApp');
+        console.log('Error reading binary file for upload to HockeyApp');
     });
 
     // HockeyApp properties
-    var HOCKEY_APP_HOST = 'rink.hockeyapp.net';
-    var HOCKEY_APP_PATH = '/api/2/apps/upload/';
-    var HOCKEY_APP_PROTOCOL = 'https:';
+    const HOCKEY_APP_HOST = 'rink.hockeyapp.net';
+    const HOCKEY_APP_PATH = `/api/2/apps/${options.hockeyappId}/app_versions`;
+    const HOCKEY_APP_PROTOCOL = 'https:';
 
     // Create FormData
-    var form = new FormData();
+    const form = new FormData();
     form.append('status', 2);
-    // form.append('mandatory', MANDATORY_TYPE[options.mandatory]);
     form.append('notes', "Automated release triggered from Unity Cloud Build.");
     form.append('notes_type', 0);
     form.append('notify', 0);
     form.append('ipa', readable);
 
-    var req = form.submit({
-      host: HOCKEY_APP_HOST,
-      path: HOCKEY_APP_PATH,
-      protocol: HOCKEY_APP_PROTOCOL,
-      headers: {
-        'Accept': 'application/json',
-        'X-HockeyAppToken': options.hockeyappAPIKey
-      }
+    const req = form.submit({
+        host: HOCKEY_APP_HOST,
+        path: HOCKEY_APP_PATH,
+        protocol: HOCKEY_APP_PROTOCOL,
+        headers: {
+            'Accept': 'application/json',
+            'X-HockeyAppToken': options.hockeyappAPIKey
+        }
     }, function (err, res) {
         if (err) {
             console.log(err);
+            return;
         }
 
         if (res.statusCode !== 200 && res.statusCode !== 201) {
             console.log('Uploading failed with status ' + res.statusCode);
-            console.log(res);
-            // res.on('data', function (chunk) {
-            //   console.log(chunk);
-            //             // res.on('end', function () {
-            //   console.log("end");
-            // });
+            console.log(res.statusMessage);
             return;
         }
 
-        var jsonString = '';
+        let jsonString = '';
         res.on('data', (chunk) => {
-        
-            jsonString += String.fromCharCode.apply(null, new Uint16Array( chunk ));
+
+            jsonString += String.fromCharCode.apply(null, new Uint16Array(chunk));
 
         });
 
@@ -208,18 +207,15 @@ function uploadToHockeyApp( filename ){
 
             console.log("3. uploadToHockeyApp: finished");
 
-            // console.log(jsonString);
-
-            deleteFile( filename );
+            deleteFile(filename);
 
         });
     });
-    
+
     // Track upload progress.
-    // console.log( req );
-    var len = parseInt( req.getHeader( 'content-length' ), 10);
-    var cur = 0;
-    var total = len / 1048576; //1048576 - bytes in  1Megabyte
+    const len = parseInt(req.getHeader('content-length'), 10);
+    let cur = 0;
+    const total = len / 1048576; //1048576 - bytes in  1Megabyte
 
     req.on('data', (chunk) => {
         cur += chunk.length;
